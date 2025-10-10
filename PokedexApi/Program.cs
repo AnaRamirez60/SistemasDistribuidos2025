97aa89ed0e2f369ddc42ba898b4405d8d4db4e0b
Ana <ana@Anas-MacBook-Air.local>
2025-10-09 23:02:58 -0600

    Authorize

--- PokedexApi/Program.cs
+++ PokedexApi/Program.cs
@@ -1,19 +1,50 @@
 using PokedexApi.Services;
 using PokedexApi.Mappers;
 using PokedexApi.Gateways;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddEndpointsApiExplorer(); 
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 builder.Services.AddScoped<IPokemonService, PokemonService>();
-builder.Services.AddScoped<IPokemonGateway, PokemonGateway>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options => 
+            {
+            options.Authority = builder.Configuration.GetValue<string>("Authentication:Authority");
+            options.TokenValidationParameters = new TokenValidationParameters 
+            {
+                ValidateIssuer = false,
+                ValidIssuer = builder.Configuration.GetValue<string>("Authentication:Issuer"),
+                ValidateActor = false,
+                ValidateLifetime = false,
+                ValidateAudience = false,
+                ValidAudience = "pokedex-api",
+                ValidateIssuerSigningKey = true
+            };
+            options.RequireHttpsMetadata = false;
+            });
+
+builder.Services.AddAuthorization(options => 
+        {
+        options.AddPolicy("Read", policy => policy.RequireClaim("http://schemas.microsoft.com/identity/claims/scope", 
+                    "read"));
+        options.AddPolicy("Write", policy => policy.RequireClaim("http://schemas.microsoft.com/identity/claims/scope", 
+                    "write"));
+        });
 
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseHttpsRedirection();
 app.MapControllers();
